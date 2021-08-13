60b6b2b3c30731f17085b1b6de291b936f4431bf
Puja Kumari <71400981+Puja-Kumari-909@users.noreply.github.com>
2021-08-13 22:32:37 +0530

    Majority element

---
+++ Milestone13/MajorityElement.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+public class MajorityElement {
+
+    static int findMajority(int[] arr){
+
+        Arrays.sort(arr);
+        int temp = arr[0];
+        int count = 1;
+        int ele = 0;
+        int max_ele = -1;
+        int flag = 0;
+
+        for(int i=1; i<arr.length; i++){
+
+            if(temp == arr[i]){
+                count++;
+            }
+
+            else{
+                count = 1;
+                temp = arr[i];
+            }
+
+            if(max_ele<count){
+                max_ele = count;
+                ele = arr[i];
+
+                if(max_ele>(arr.length/2)){
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+        return (flag==1? ele : -1);
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 1, 4, 1};
+        
+        int ans = findMajority(arr);
+        if(ans==-1){
+            System.out.println("No such Element");
+        }
+        else{
+            System.out.println("Majority element is "+ ans);
+        }
+    }
+}
