722ac616ff6f73db544c187f0de296be0780c967
Puja Kumari <71400981+Puja-Kumari-909@users.noreply.github.com>
2021-10-05 14:33:26 +0530

    Update Kadane.java

--- Algorithms/Kadane.java
+++ Algorithms/Kadane.java
@@ -18,3 +18,21 @@ public class Kadane {
         System.out.println(max_sum);
     }
 }
+
+//Kadane's Algorithm for finding maximum sum subarray if all the elements are negative
+
+public class Kadane {
+    public static void main(String[] args) {
+        int[] arr = {-5, -2, -4, -1, -2, -5, -2, -6, -9};
+
+        int max_sum = arr[0];
+        int sum =arr[0];
+
+        for(int i=1; i<arr.length; i++){
+           sum = Math.max(arr[i], sum+arr[i]);
+           max_sum = Math.max(sum, max_sum);
+        }
+
+        System.out.println(max_sum);
+    }
+}
